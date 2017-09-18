e0226a1f604fe6de022849d8a0571898b085190f
David Keller <email": null>
2017-09-18 09:59:37 -0700

    Add subscribe functionality

---
+++ app/controllers/subscriptions.rb
@@ -0,0 +1,28 @@
+# Subscription create action => POST /subscriptions
+post '/subscriptions' do
+  @user = current_user
+  @pokemon = Pokemon.find(params[:pokemon_id])
+
+  @subscription = Subscription.new(user: @user, pokemon: @pokemon)
+  if @subscription.save
+    redirect '/profile'
+  else
+    status 422
+    @pokemon = Pokemon.find(params[:pokemon_id])
+    @errors = @subscription.errors.full_messages
+    erb :'pokemon/show'
+  end
+end
+
+# Subscriptions destroy action => DELETE /subscriptions/:id
+delete '/subscriptions/:id' do
+  redirect '/login' unless logged_in?
+  subscription = Subscription.find(params[:id])
+  if subscription.user == current_user
+    subscription.destroy
+    redirect '/profile'
+  else
+    status 403
+    redirect '/'
+  end
+end
