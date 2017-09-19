9129bfebac30238b8f36cbf1906710e15cc3e9ce
David Keller <email": null>
2017-09-18 21:22:34 -0700

    Add final elements

--- app/controllers/pokemon.rb
+++ app/controllers/pokemon.rb
@@ -8,6 +8,7 @@ end
 get '/pokemon/show' do
   @user = User.find(session[:user_id])
   @pokemon = Pokemon.find_by(name: params[:name])
+  @reviews = @pokemon.reviews
 
   if request.xhr?
     erb :"pokemon/_show", layout: false
